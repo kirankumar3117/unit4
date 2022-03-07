60705dd73ce109db0d6431ed4f228a3574c408f2
kirankumar3117 <ch.kirankumar311@gmail.com>
2022-03-07 13:35:45 +0530

    Update index.js

--- unit4_c1/index.js
+++ unit4_c1/index.js
@@ -8,31 +8,48 @@ app.get("/books",(req,res)=>{
     return res.send({route:"/books"})
 })
 
-app.use(checkPermission);
+app.use(checkpermission);
 
-app.get("/libraries",(req,res)=>{
+app.get("/libraries",checkPermission("librarian"), (req,res)=>{
     return res.send({ route: "/libraries", permission: true})
 })
 
-app.get("/authors",(req,res)=>{
+app.get("/authors",checkPermission("author"),(req,res)=>{
     return res.send({ route: "/authors", permission: true})
 })
 
-  function checkPermission(req,res,next){
+function checkPermission(role){
+ return function checkpermission(req,res,next){
     console.log("Before bigining the checkPermission");
-   if(req.path === "/libraries"){
+   if(role === "librarian"){
+       req.role = "true"
     return next();
 
    }
-   else if(req.path==="/authors"){
+  
+   else if(role === "author"){
+      
+       req.role="true";
        return next();
    }
-   else{
+   else {
        return res.send("Permission Not Allowed")
    }
-   
-  
+}
+}
 
+function checkpermission(req,res,next){
+    if( req.path === "/librarian"){
+        return res.send({ route: "/libraries", permission: true})
+    }
+    else if(req.path == "/author"){
+        return res.send({ route: "/authors", permission: true})
+    }
+    else{
+        next()
+        return res.send("Permission Not Allowed")
+    }
+  
 }
 
 
@@ -43,6 +60,6 @@ function logger(req,res,next){
     next();
     console.log("End Of The logger Function")
 }
-app.listen(5003,()=>{
-    console.log("Iam Listening Port 5003")
+app.listen(8000,()=>{
+    console.log("Iam Listening Port 8000")
 })
